4759312c9b72aee3546683cf7dfbc7a56648d826
denis <denisfilinov@yandex.ru>
2023-11-02 16:10:20 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://c8ddb5a2-4914-4c44-b853-484354e33c05.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,11 @@
+import configuration
+import requests
+import data
+
+
+
+
+
+
+
+
